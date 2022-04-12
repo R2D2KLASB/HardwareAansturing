83126db85d90bdabe7e518d6c1f53176d7353813
marhotiewer <marhotiewer@protonmail.com>
2022-04-12 09:03:08 +0000

    ros2 basic node integration

---
+++ ros2/build/rpi_serial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/HardwareAansturing/ros2/rpi_serial
+# Build directory: /home/ubuntu/HardwareAansturing/ros2/build/rpi_serial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
