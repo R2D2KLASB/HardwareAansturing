32a580a80acf2741ff628d52cba8b330fecaaa19
Jasper <jaspervdpost@gmail.com>
2022-05-20 11:19:33 +0200

    some additions for conversion to platformio

--- XY_Plotter/include/Definitions.hpp
+++ XY_Plotter/include/Definitions.hpp
@@ -1,6 +1,7 @@
 #ifndef DEFINITIONS_HPP
 #define DEFINITIONS_HPP
 
+#define BAUDRATE 19200
 
 #define ENABLE_PIN 8
 #define DIR_X_PIN 5
@@ -10,10 +11,10 @@
 #define STEP_Y_PIN 3
 
 #define MAX_X 30000
-#define MAX_Y 25000
+#define MAX_Y 25000 
 
 #define SERVO_PIN 11
-#define SERVO_UP 0
+#define SERVO_UP 40
 #define SERVO_DOWN 90
 
 #define MICROSWITCHX 9
--- XY_Plotter/src/main.cpp
+++ XY_Plotter/src/main.cpp
@@ -1,14 +1,39 @@
 #include <Servo.h>
-#include <Arduino.h>
-
-#include "XYplotter.hpp"
+#include "XYPlotter.hpp"
 #include "DataTypes.hpp"
 #include "Definitions.hpp"
 #include "queue.hpp"
 
+// #define DEBUG
+
 Servo pen;
 XYPlotter plot({MAX_X, MAX_Y}, pen, ENABLE_PIN, DIR_X_PIN, STEP_X_PIN,MICROSWITCHX, DIR_Y_PIN, STEP_Y_PIN, MICROSWITCHY);
+
 Queue queue;
+
+#ifdef DEBUG
+void print_draw(Coordinate xy, int mode) {
+ SerialUSB.print("DRAW x: ");
+ SerialUSB.print(xy.x);
+ SerialUSB.print(" Y: ");
+ SerialUSB.print(xy.y);
+ SerialUSB.print(" int mode : ");
+ SerialUSB.println(mode);
+}
+#endif // DEBUG
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  // start serial port
+  Serial.begin(BAUDRATE);
+
+  #ifdef DEBUG
+  SerialUSB.begin(BAUDRATE);
+  #endif // DEBUG
+
+  // call init function for plotter
+  plot.init();
+}
 
 void SerialFlush() {
   while (Serial.available() > 0) {
@@ -16,36 +41,35 @@ void SerialFlush() {
   }
 }
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  // start serial port at 19200 bps:
-  Serial.begin(115200);
-  plot.init();
-}
-
-
-
 void loop() {
   Gcode readCode;
   readCode.gcode = 0;
-  // if we get a valid byte, read analog ins:
+  // clear buffer for random existing bytes
   SerialFlush();
+
+  // write byte to start a handshake
   Serial.write(6);
+
+  // while not exit gcode
   while (readCode.gcode != -1) {
+
+    // wait for reply
     while (Serial.available() == 0) {}
-    readCode.gcode = Serial.parseFloat();
+    readCode.gcode = Serial.parseInt();
+
+    // check if gcode is not equal to exit
     if (readCode.gcode != -1) {
       switch (readCode.gcode) {
         //G00 Z-axis up and move to location (x, y)
         case 0:
-          readCode.location.x = Serial.parseFloat();
-          readCode.location.y = Serial.parseFloat();
+          readCode.location.x = Serial.parseInt();
+          readCode.location.y = Serial.parseInt();
           break;
 
         //G01 Z-axis down and move to location (draw line) (x, y)
         case 1:
-          readCode.location.x = Serial.parseFloat();
-          readCode.location.y = Serial.parseFloat();
+          readCode.location.x = Serial.parseInt();
+          readCode.location.y = Serial.parseInt();
           break;
         case 28:
           readCode.location.x = 0;
@@ -59,6 +83,7 @@ void loop() {
         break;
       }
       else {
+        // write byte to start a handshake
         Serial.write(6);
       }
     }
@@ -69,11 +94,21 @@ void loop() {
       //G00 Z-axis up and move to location (x, y)
       case 0:
         plot.draw(writeCode.location, 0);
+        
+        #ifdef DEBUG
+        print_draw(writeCode.location, 0);
+        #endif // DEBUG
+
         break;
 
       //G01 Z-axis down and move to location (draw line) (x, y)
       case 1:
         plot.draw(writeCode.location, 1);
+
+        #ifdef DEBUG
+        print_draw(writeCode.location, 1);
+        #endif // DEBUG
+
         break;
 
       //G28 home
