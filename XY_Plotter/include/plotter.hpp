d1024bc20f53daef0f331d238dd1029d737eda81
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-06-20 10:36:03 +0200

    Updated xy-plotter code to support updates to virtual class

--- XY_Plotter/include/plotter.hpp
+++ XY_Plotter/include/plotter.hpp
@@ -66,17 +66,6 @@ class Plotter {
      */
     void g6(const int& row, const int& colom, const int& width, const int& length);
 
-
-    /**
-     * @brief function that draws a sunken boat on the game board
-     *
-     * @param row the x-location on the battleship game board
-     * @param colom the y-location on the battleship game board
-     * @param width the length of the boat over the x-axis
-     * @param length the length of the boat over the y-axis
-     */
-    void g7(const int& row, const int& colom, const int& width, const int& length, const int& player);
-
     /**
       @brief Function to return to the homeposition using the microswitches
       @details Homing function that uses the microswitches to return to the homeposition over x and y-axis and than resets than calibrates the position.
@@ -85,15 +74,15 @@ class Plotter {
   protected:
     bool prevState = false;
     Coordinate currentLocation = {0,0};
-    const Coordinate enemyGameboardOrigin = {11000, 0};
+    const Coordinate enemyGameboardOrigin = {21000, 0};
     const Coordinate friendlyGameboardOrigin = {0, 0};
     Coordinate maxDimension;
 
     const float pi = 3.14159265358979323846;
 
 
-    const int friendlyGameboardSize = 10000;
-    const int enemyGameboardSize = 10000;
+    const int friendlyGameboardSize = 20000;
+    const int enemyGameboardSize = 5000;
 
     unsigned int stepDelayUs = 50;
     unsigned int servoDelayUs = 150000;
@@ -105,6 +94,6 @@ class Plotter {
 
     virtual void setServo(bool draw) = 0;
 
-    Coordinate pointsOnCircle(int radius, int angle, Coordinate origin);
+    Coordinate pointsOnCircle(const int& radius, const int& angle, const Coordinate& origin);
 
 };
