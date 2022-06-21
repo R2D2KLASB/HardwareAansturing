adb9688f889fbcd1b3501bdb5e245f978a29be1e
Skaniel <Dhoegee@live.com>
2022-06-21 12:05:46 +0200

    Background sound stops playing when win or lose

--- Infrastructuur-feature-working-nodes/src/battleship/include/sound_player_node.hpp
+++ Infrastructuur-feature-working-nodes/src/battleship/include/sound_player_node.hpp
@@ -8,9 +8,10 @@ class sound_player_node : public rclcpp::Node {
 public:
     sound_player_node() : Node("sound_player_node") {
         subscription_ = this->create_subscription<std_msgs::msg::String>(
-                "GameInfo", 10, std::bind(&sound_player_node::topic_callback, this, _1));
+                "speaker", 10, std::bind(&sound_player_node::topic_callback, this, _1));
     }
-    void play_sound(std::string file_name) const;
+    void play_game_sound(std::string file_name) const;
+    void play_background_sound(std::string file_name) const;
 
 private:
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const;
