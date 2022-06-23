34991f2804abaefa7fc8dda951386323a14b5e8a
Joshua-Veens <78480354+Joshua-Veens@users.noreply.github.com>
2022-06-23 15:06:42 +0200

    Update Controls PULLUP input

--- Display & Controls/controls.hpp
+++ Display & Controls/controls.hpp
@@ -1,35 +1,35 @@
 class controls {
 private:
   const int fire = 41;
-  const int up = 37;
-  const int down = 35;
-  const int right = 33;
-  const int left = 39;
+  const int up = 35;
+  const int down = 37;
+  const int right = 39;
+  const int left = 33;
 
 public:
   void setupControls(){
     // Fire button and joystick init
-    pinMode(fire, INPUT);
-    pinMode(up, INPUT);
-    pinMode(down, INPUT);
-    pinMode(right, INPUT);
-    pinMode(left, INPUT);
+    pinMode(fire, INPUT_PULLUP);
+    pinMode(up, INPUT_PULLUP);
+    pinMode(down, INPUT_PULLUP);
+    pinMode(right, INPUT_PULLUP);
+    pinMode(left, INPUT_PULLUP);
   }
 
   int getPosition(){
-    if ( digitalRead(fire) ){
+    if ( digitalRead(fire) == LOW ){
       return 0;
     }
-    if ( digitalRead(up) ){
+    if ( digitalRead(up) == LOW ){
       return 1;
     }
-    if ( digitalRead(down) ){
+    if ( digitalRead(down) == LOW ){
       return 2;
     }
-    if ( digitalRead(left) ){
+    if ( digitalRead(left) == LOW ){
       return 3;
     }
-    if ( digitalRead(right) ){
+    if ( digitalRead(right) == LOW ){
       return 4;
     }
     return -1;
