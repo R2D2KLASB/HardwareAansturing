9aac7bf9c4e438b94bba4fa300c30f4e6feacebd
StijnOost <60661227+StijnOost@users.noreply.github.com>
2022-06-24 14:10:22 +0200

    Delete ros2 directory

--- ros2/build/rpi_serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/HardwareAansturing/ros2/rpi_serial
-# Build directory: /home/ubuntu/HardwareAansturing/ros2/build/rpi_serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
