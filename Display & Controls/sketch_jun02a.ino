e4d1362077b3e7c8f2fb1b8b044ad6f95d7a14ce
Jasper <jaspervdpost@gmail.com>
2022-06-28 14:52:04 +0200

    reverted a wrong commit

--- Display & Controls/sketch_jun02a.ino
+++ Display & Controls/sketch_jun02a.ino
@@ -1,49 +1,43 @@
-#include "display.hpp"
-#include "controls.hpp"
-
-TFTdisplay TFT;
-controls control;
-
 void setup() {
   // TFT screen setup and SerialUSB
   TFT.setupScreen();
-  SerialUSB.begin(9600);
+  Serial.begin(9600);
 
   // Joystick and fire button setup and Serial
-  Serial.begin(9600);
+  SerialUSB.begin(9600);
   control.setupControls();
 }
 
-void loop() {
-  // Serial for Control
+	@@ -19,32 +19,31 @@ void loop() {
   Serial.println(control.getPosition());
 
   // Serial for Display
-  int message = SerialUSB.read();
-  
-  if ( message == '6' ){
-    String string = "\n";
-    while ( string == "\n" ){
-      string = SerialUSB.readString();
+  while (SerialUSB.available() > 0) {
+    int message = SerialUSB.parseInt();
+
+    if ( message == 6) {
+      String string = SerialUSB.readString();
+      TFT.setText( string );
     }
-    TFT.setText( string );
+
+    if ( message == 0 ) { // 0 = removeBoats
+      TFT.removeBoats();
+    }
+    else if ( message == 1 ) { // 1 = addHit
+      TFT.addHit();
+    }
+    else if ( message == 2 ) { // 2 = addMiss
+      TFT.addMiss();
+    }
+    else if ( message == 3 ) { // 3 = removeEnemyBoats
+      TFT.removeEnemyBoats();
+    }
+    else if ( message == 4 ) { // 4 = addEnemyHit
+      TFT.addEnemyHit();
+    }
+    else if ( message == 5 ) { // 5 = addEnemyMiss
+      TFT.addEnemyMiss();
+    }
   }
-  if ( message == '0' ){ // 0 = removeBoats
-    TFT.removeBoats();       
-  }
-  else if ( message == '1' ){ // 1 = addHit
-    TFT.addHit();
-  }
-  else if ( message == '2' ){ // 2 = addMiss
-    TFT.addMiss(); 
-  }
-  else if ( message == '3' ){ // 3 = removeEnemyBoats
-    TFT.removeEnemyBoats();
-  }
-  else if ( message == '4' ){ // 4 = addEnemyHit
-    TFT.addEnemyHit();
-  }
-  else if ( message == '5' ){ // 5 = addEnemyMiss
-    TFT.addEnemyMiss();
-  }
+
 }
