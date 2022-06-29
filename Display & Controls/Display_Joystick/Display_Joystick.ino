00068f253d00c9eca86e9bc2611b6c20b10cfb52
Skaniel <Dhoegee@live.com>
2022-06-29 13:34:39 +0200

    update arduino code for display and joystick

--- Display & Controls/Display_Joystick/Display_Joystick.ino
+++ Display & Controls/Display_Joystick/Display_Joystick.ino
@@ -16,35 +16,34 @@ void setup() {
 
 void loop() {
   Serial.println(control.getPosition());
-  
+
   // Serial for Display
   while (SerialUSB.available() > 0) {
     int message = SerialUSB.parseInt();
 
-    if ( message == 6) {
+    if ( message == 1 ) { // 1 = removeBoats
+      TFT.removeBoats();
+    }
+    else if ( message == 2 ) { // 2 = addHit
+      TFT.addHit();
+    }
+    else if ( message == 3 ) { // 3 = addMiss
+      TFT.addMiss();
+    }
+    else if ( message == 4 ) { // 4 = removeEnemyBoats
+      TFT.removeEnemyBoats();
+    }
+    else if ( message == 5 ) { // 5 = addEnemyHit
+      TFT.addEnemyHit();
+    }
+    else if ( message == 6 ) { // 6 = addEnemyMiss
+      TFT.addEnemyMiss();
+    }
+    else if ( message == 7) { // 7 = display text
       String string = SerialUSB.readString();
       TFT.setText( string );
     }
-
-    if ( message == 0 ) { // 0 = removeBoats
-      TFT.removeBoats();
-    }
-    else if ( message == 1 ) { // 1 = addHit
-      TFT.addHit();
-    }
-    else if ( message == 2 ) { // 2 = addMiss
-      TFT.addMiss();
-    }
-    else if ( message == 3 ) { // 3 = removeEnemyBoats
-      TFT.removeEnemyBoats();
-    }
-    else if ( message == 4 ) { // 4 = addEnemyHit
-      TFT.addEnemyHit();
-    }
-    else if ( message == 5 ) { // 5 = addEnemyMiss
-      TFT.addEnemyMiss();
-    }
-    else if ( message == 7 ) { // 7 = reset game to 0
+    else if ( message == 8 ) { // 8 = reset game to 0
       control.resetEverything();
     }
   }
